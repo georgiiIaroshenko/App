75aca615a7ae26bed02d16dbfda6593c9e1bc6c5
georgiiIaroshenko <georg2991@mail.ru>
2025-01-20 20:46:18 +0300

    no message

---
+++ App/Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "App",
+    platforms: [
+            .iOS(.v16)
+        ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "App",
+            targets: ["App"]),
+    ],
+    dependencies: [],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "App",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "AppTests",
+            dependencies: ["App"]
+        ),
+    ]
+)
